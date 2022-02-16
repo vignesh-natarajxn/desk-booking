cf16f8d250e0dcd20f1bda7fcd754f44423a5122
viknhat <viknhat@gmail.com>
2022-02-16 11:23:59 +0530

    more desks

--- src/App.js
+++ src/App.js
@@ -25,6 +25,86 @@ const SLOT_DETAILS = [
     name: "Desk 4",
     bookingStatus: false
   },
+  {
+    id: "d5",
+    name: "Desk 5",
+    bookingStatus: false
+  },
+  {
+    id: "d6",
+    name: "Desk 6",
+    bookingStatus: false
+  },
+  {
+    id: "d7",
+    name: "Desk 7",
+    bookingStatus: false
+  },
+  {
+    id: "d8",
+    name: "Desk 8",
+    bookingStatus: false
+  },
+  {
+    id: "d9",
+    name: "Desk 9",
+    bookingStatus: false
+  },
+  {
+    id: "d10",
+    name: "Desk 10",
+    bookingStatus: false
+  },
+  {
+    id: "d11",
+    name: "Desk 11",
+    bookingStatus: false
+  },
+  {
+    id: "d12",
+    name: "Desk 12",
+    bookingStatus: false
+  },
+  {
+    id: "d13",
+    name: "Desk 13",
+    bookingStatus: false
+  },
+  {
+    id: "d14",
+    name: "Desk 14",
+    bookingStatus: false
+  },
+  {
+    id: "d15",
+    name: "Desk 15",
+    bookingStatus: false
+  },
+  {
+    id: "d16",
+    name: "Desk 16",
+    bookingStatus: false
+  },
+  {
+    id: "d17",
+    name: "Desk 17",
+    bookingStatus: false
+  },
+  {
+    id: "d18",
+    name: "Desk 18",
+    bookingStatus: false
+  },
+  {
+    id: "d19",
+    name: "Desk 19",
+    bookingStatus: false
+  },
+  {
+    id: "d20",
+    name: "Desk 20",
+    bookingStatus: false
+  }
 ];
 
 const App = () => {
